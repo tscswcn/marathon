1352ae1c042f822a017a18ed202106c467b27743
Karsten Jeschkies <kjeschkies@mesosphere.com>
2018-08-13 15:12:45 +0200

    Define second level scheduler interface.
    Summary:
    The orchestration layer should only communicate via the scheduler
    interface. The instance tracker, launch queue and kill service will be
    hidden behind that. This diff defines said interface and implements a
    legacy scheduler that just forwards all requests to the underlying
    modules.
    
    JIRA issues: MARATHON-8296

--- src/main/scala/mesosphere/marathon/core/CoreModuleImpl.scala
+++ src/main/scala/mesosphere/marathon/core/CoreModuleImpl.scala
@@ -38,6 +38,7 @@ import mesosphere.marathon.core.task.jobs.TaskJobsModule
 import mesosphere.marathon.core.task.termination.TaskTerminationModule
 import mesosphere.marathon.core.task.tracker.InstanceTrackerModule
 import mesosphere.marathon.core.task.update.TaskStatusUpdateProcessor
+import mesosphere.marathon.scheduling.SchedulingModule
 import mesosphere.marathon.storage.{StorageConf, StorageConfig, StorageModule}
 import mesosphere.marathon.stream.EnrichedFlow
 import mesosphere.util.NamedExecutionContext
@@ -221,6 +222,12 @@ class CoreModuleImpl @Inject() (
     () => marathonScheduler.getLocalRegion
   )
 
+  lazy val schedulingModule = new SchedulingModule(
+    launcherModule.offerProcessor,
+    instanceTrackerModule.instanceTracker,
+    taskStatusUpdateProcessor
+  )
+
   // PLUGINS
   override lazy val pluginModule = new PluginModule(marathonConf, crashStrategy)
 
@@ -283,7 +290,7 @@ class CoreModuleImpl @Inject() (
     metricsModule.metrics,
     marathonConf,
     leadershipModule,
-    instanceTrackerModule.instanceTracker,
+    schedulingModule,
     taskTerminationModule.taskKillService,
     appOfferMatcherModule.launchQueue,
     schedulerActions, // alternatively schedulerActionsProvider.get()
@@ -339,7 +346,7 @@ class CoreModuleImpl @Inject() (
     eventStream,
     taskTerminationModule.taskKillService)(schedulerActionsExecutionContext)
 
-  override lazy val marathonScheduler: MarathonScheduler = new MarathonScheduler(eventStream, launcherModule.offerProcessor, taskStatusUpdateProcessor, storageModule.frameworkIdRepository, mesosLeaderInfo, marathonConf, crashStrategy)
+  override lazy val marathonScheduler: MarathonScheduler = new MarathonScheduler(eventStream, schedulingModule.scheduler, storageModule.frameworkIdRepository, mesosLeaderInfo, marathonConf, crashStrategy)
 
   // MesosHeartbeatMonitor decorates MarathonScheduler
   override def mesosHeartbeatMonitor = new MesosHeartbeatMonitor(marathonScheduler, heartbeatActor)
